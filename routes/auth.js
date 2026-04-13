f6b25cd2e5f44bef8759bafc03d176e1ca7b0b44
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-04-13 14:37:06 -0500

    Agregar cors para url del front + actualizando agregando flujo autenticacion

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,35 @@
 import express from 'express';
-import { registro } from '../controllers/authController.js';
+import {
+  registro,
+  verificarCuenta,
+  reenviarCodigo,
+  login,
+  solicitarReset,
+  verificarReset,
+  nuevaContrasena,
+  perfil,
+  actualizarPerfil,
+  cambiarContrasena } from '../controllers/authController.js';
+import { auth } from '../middleware/auth.js'
 
 const router = express.Router();
 
+/** Registro y verificacion */
 router.post('/registro', registro);
+router.post('/verificar-cuenta', verificarCuenta);
+router.post('/reenviar-codigo', reenviarCodigo);
+
+/** Login */
+router.post('/login', login);
+
+/** Reestablecimiento de contrasena */
+router.post('/solicitar-reset', solicitarReset);
+router.post('/verificar-reset', verificarReset);
+router.post('/nueva-contrasena', nuevaContrasena);
+
+/** PERFIL (requiere autenticación) */
+router.get('/perfil', auth, perfil);
+router.put('/perfil', auth, actualizarPerfil);
+router.put('/cambiar-contrasena', auth, cambiarContrasena);
 
 export default router;
