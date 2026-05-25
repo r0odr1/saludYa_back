07881f7a69489d93f44cc14a174e0fccfb829952
Rodrigo Bonilla <rhbon.08@gmail.com>
2026-05-25 13:42:23 -0500

    fix: soportar puerto 465 dinamicamente

--- utils/email.js
+++ utils/email.js
@@ -9,7 +9,7 @@ const crearTransporter = () => {
   return nodemailer.createTransport({
     host: process.env.EMAIL_HOST || 'smtp.gmail.com',
     port: parseInt(process.env.EMAIL_PORT) || 587,
-    secure: false,
+    secure: parseInt(process.env.EMAIL_PORT) === 465,
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS
